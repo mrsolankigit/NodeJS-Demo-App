eb65b53afd2c63f43eec8626c51c5c6baf8c922d
Valentine <blessador@gmail.com>
2024-09-03 09:34:58 +0100

    Add simple test with Mocha

---
+++ test/app.test.js
@@ -0,0 +1,9 @@
+// test/app.test.js
+
+const assert = require('assert');
+
+describe('Simple Test', function() {
+    it('should return true', function() {
+        assert.strictEqual(true, true);
+    });
+});
